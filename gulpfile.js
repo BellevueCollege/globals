8774f6d1ab909da1dec2b52bc15877c9b2b230c4
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2017-03-01 15:33:55 -0800

    Create -full head and foot js files

--- gulpfile.js
+++ gulpfile.js
@@ -63,6 +63,22 @@ var uglifyOptions = {
   }
 }
 
+var modernizrOptions = {
+  "tests": [
+	"displaytable",
+	"flexbox",
+	"flexboxlegacy",
+	"flexboxtweener",
+	"flexwrap",
+	"fontface"
+  ],
+  "options": [
+	"testStyles",
+	"html5shiv",
+	"setClasses"
+  ],
+}
+
 /**
  * Sass Compilers
  *
@@ -117,21 +133,7 @@ gulp.task('modernizr-dev', function() {
       './javascripts/**.js',
       './sass/**.scss'
     ])
-    .pipe(modernizr('ghead.js', {
-      "tests": [
-        "displaytable",
-        "flexbox",
-        "flexboxlegacy",
-        "flexboxtweener",
-        "flexwrap",
-        "fontface"
-      ],
-      "options": [
-        "testStyles",
-        "html5shiv",
-        "setClasses"
-      ],
-    }))
+    .pipe(modernizr('ghead.js', modernizrOptions))
     .pipe(gulp.dest(config.verPath + '/j'));
 });
 
@@ -167,21 +169,7 @@ gulp.task('modernizr', function() {
       './javascripts/**.js',
       './sass/**.scss'
     ])
-    .pipe(modernizr('ghead.js', {
-      "tests": [
-        "displaytable",
-        "flexbox",
-        "flexboxlegacy",
-        "flexboxtweener",
-        "flexwrap",
-        "fontface"
-      ],
-      "options": [
-        "testStyles",
-        "html5shiv",
-        "setClasses"
-      ],
-    }))
+    .pipe(modernizr('ghead.js', modernizrOptions))
     .pipe(uglify(uglifyOptions))
     .pipe(gulp.dest(config.verPath + '/j'));
 });
@@ -208,6 +196,42 @@ gulp.task('globals-footer-scripts', function() {
 });
 
 
+// Fully concatinated header scripts
+
+gulp.task('header-scripts-full', ['modernizr'], function() {
+  return gulp
+    .src([
+      config.verPath + '/j/ghead.js',
+      config.npmPath + '/Respond.js/dest/respond.src.js'
+    ])
+    .pipe(concat('ghead-full.js'))
+    .pipe(gulp.dest(config.verPath + '/j'));
+});
+
+// Fully concatinated footer scripts
+gulp.task('footer-scripts-full', function() {
+  return gulp
+    .src([
+      config.npmPath + '/bootstrap-sass/assets/javascripts/bootstrap.js',
+      config.npmPath + '/bootstrap-accessibility-plugin/plugins/js/bootstrap-accessibility.js',
+      config.npmPath + '/noisy/jquery/jquery.noisy.js',
+      config.jsPath + '/noisy-calls.js'
+    ])
+    .pipe(concat('gfoot-full.js'))
+    .pipe(gulp.dest(config.verPath + '/j'));
+});
+
+// Minify fully concatinated scripts
+gulp.task('minify-full', ['header-scripts-full', 'footer-scripts-full'], function() {
+  return gulp
+    .src([
+      config.verPath + '/j/ghead-full.js',
+      config.verPath + '/j/gfoot-full.js'
+    ])
+    .pipe(uglify(uglifyOptions))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(config.verPath + '/j'));
+});
 
 
 // Watch function (sass) - dev use only
@@ -217,6 +241,7 @@ gulp.task('watch',function() {
 });
 
 
+
 // Dev - full dev build
 gulp.task('dev', [
             'sass-dev',
@@ -224,6 +249,9 @@ gulp.task('dev', [
             'bootstrap-scripts',
             'modernizr-dev',
             'respondjs',
+            'header-scripts-full',
+            'footer-scripts-full',
+            'minify-full',
             'globals-footer-scripts-dev',
           ]);
 
@@ -234,5 +262,8 @@ gulp.task('default', [
             'bootstrap-scripts',
             'modernizr',
             'respondjs',
+            'header-scripts-full',
+            'footer-scripts-full',
+            'minify-full',
             'globals-footer-scripts',
           ]);
