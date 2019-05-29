6ae38d75f70c78e528aaaa4e16316df15fe87df2
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-05-29 14:24:50 -0700

    🐛 FIX: Alternate LocalStorage Keys
    Fix GLOB-16. Consistently use configured local storage key instead of
    assuming the default should be used. Whoops.

--- g/3/j/gfoot-full.js
+++ g/3/j/gfoot-full.js
@@ -3340,13 +3340,13 @@ CC0: http://creativecommons.org/publicdomain/zero/1.0/
 				localStorage.setItem(options.localStorageKey, JSON.stringify( searchHistory ));
 			} else {
 				// Load previous search history and parse to JSON
-				searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+				searchHistory = JSON.parse(localStorage.getItem(options.localStorageKey));
 				// Add new search term to array
 				searchHistory.unshift(historyObject);
 				// Truncate array to only save 5 items
 				searchHistory = searchHistory.slice(0, 5);
 				// Build array and save to local storage as JSON array in string
-				localStorage.searchHistory = JSON.stringify( searchHistory );
+				localStorage.setItem(options.localStorageKey, JSON.stringify( searchHistory ));
 			}
 			
 		}
